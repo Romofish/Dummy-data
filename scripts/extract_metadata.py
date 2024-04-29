eb07df2ce66cd31b50847904a023ee83c6251703
Flora_fttm <flora-1.chen@novartis.com>
2024-04-29 14:47:52 +0800

    update the dev branch

--- scripts/extract_metadata.py
+++ scripts/extract_metadata.py
@@ -2,25 +2,30 @@ import pandas as pd
 import json
 from datetime import datetime
 
-def extract_codelist_to_json(excel_path, output_json_path):
+def extract_variables_metadata(excel_path, output_json_path):
     # Load the Excel sheet
     sheet_name = 'Variables'
     df = pd.read_excel(excel_path, sheet_name=sheet_name)
 
-    # Organizing data hierarchically
+    # Filter out 'Role' and 'CDISC Notes' columns
+    relevant_columns = [col for col in df.columns if col not in ['Role', 'CDISC Notes']]
+
+    # Initialize hierarchical data
     hierarchical_data = {}
-    for _, row in df.dropna(subset=['Codelist Name', 'Codelist()', 'CDISC Submission Value']).iterrows():
-        codelist_name = row['Codelist Name']
-        codelist = row['Codelist()']
-        cdisc_value = row['CDISC Submission Value']
 
-        if codelist_name not in hierarchical_data:
-            hierarchical_data[codelist_name] = {}
-        
-        if codelist not in hierarchical_data[codelist_name]:
-            hierarchical_data[codelist_name][codelist] = []
+    # Group by 'Domain' and iterate over each group
+    for domain, group in df.groupby('Domain'):
+        domain_metadata = {}
 
-        hierarchical_data[codelist_name][codelist].append(cdisc_value)
+        # Iterate over each row in the group
+        for _, row in group.iterrows():
+            variable_name = row['Variable Name']  # Corrected column name
+            # Exclude 'Domain' and 'Variable Name' columns from variable metadata
+            variable_metadata = {col: row[col] for col in relevant_columns if col not in ['Domain', 'Variable Name']}
+            domain_metadata[variable_name] = variable_metadata
+
+        # Add domain metadata to hierarchical data
+        hierarchical_data[domain] = domain_metadata
 
     # Adding a header with metadata
     metadata = {
@@ -38,6 +43,6 @@ def extract_codelist_to_json(excel_path, output_json_path):
 
 # Example usage of the function
 if __name__ == "__main__":
-    excel_path = 'metadata/definitions/SDTMIG Metadata.xlsx'  # Modify with the path to your Excel file
-    output_json_path = excel_path.replace('.xlsx', '_metadata_codelist.json')  # Saves JSON in the same location
-    extract_codelist_to_json(excel_path, output_json_path)
+    excel_path = 'metadata/definitions/SDTMIG Metadata.xlsx'  # Replace with the path to your Excel file
+    output_json_path = excel_path.replace('.xlsx', '_metadata_variables.json')  # Saves JSON in the same location
+    extract_variables_metadata(excel_path, output_json_path)
